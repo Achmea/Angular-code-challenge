f9e72980c4569d5b3c1333d699e6c249fa88f923
Renee de Kruijf <renee.dekruijf@ikbenmelle-en-co.nl>
2022-09-03 14:37:44 +0200

    subtype is shown based on vehicle type

--- src/app/carform/carform.component.ts
+++ src/app/carform/carform.component.ts
@@ -6,7 +6,33 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './carform.component.html',
   styleUrls: ['./carform.component.css']
 })
+
 export class CarformComponent {
+  subTypes: any;
+
+  carSubTypes = [
+    'Hatchback',
+    'Sedan',
+    'Station',
+    'Cabriolet',
+    'Coupé',
+    'Multi Purpose Vehicle (MVP)',
+    'Terreinauto'
+  ];
+
+  motorSubTypes = [
+    'All-road',
+    'Naked',
+    'Enduro',
+    'Race',
+    'Toermotor',
+    'Chopper',
+    'Zijspan',
+  ];
+
+  scooterSubTypes = [
+    'geen subtype aanwezig'
+  ];
 
   constructor() { }
 
@@ -14,6 +40,15 @@ export class CarformComponent {
     vehicleKind: new FormControl(''),
     vehicleType: new FormControl(''),
   });
+
+  populateSubtype() {
+   if(this.carForm.controls.vehicleKind.value === 'Auto') this.subTypes = this.carSubTypes;
+   if(this.carForm.controls.vehicleKind.value === 'Motor') this.subTypes = this.motorSubTypes;
+   if(this.carForm.controls.vehicleKind.value === 'Scooter') {
+     this.subTypes = this.scooterSubTypes;
+    //  this.carForm.controls.vehicleType.disable();
+   };
+  }
 
   onSubmit() {
     console.log(this.carForm.value);
